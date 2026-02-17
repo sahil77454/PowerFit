3f627fdf3aad9a1226b2b97a8b8f7c4209f2bc86
SAHIL NARKAR <Sahilnarkar79@gmail.com>
2026-02-17 21:10:00 +0530

    Fixed selenium port

--- seleniumTest.js
+++ seleniumTest.js
@@ -2,7 +2,7 @@ const { Builder } = require('selenium-webdriver');
 
 (async function test() {
     let driver = await new Builder().forBrowser('chrome').build();
-    await driver.get('http://localhost:3000');   // your running PowerFit app
+    await driver.get('http://localhost:5000');   // your running PowerFit app
     console.log("PowerFit opened successfully");
     await driver.quit();
 })();
